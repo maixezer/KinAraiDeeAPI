8def6be3cbcf89f4ed2a54ca211b9c7754262f58
maixezer <maixezer@gmail.com>
2016-05-06 00:21:22 +0700

    Create HistoryController, and add validate of history

---
+++ app/controllers/api/v1/histories_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::HistoriesController < Api::V1::ApiController
+  before_action :authenticate_user!
+
+  load_and_authorize_resource
+
+  def index
+    render json: @histories, status: :ok
+  end
+
+  def show
+    render json: @history, status: :ok
+  end
+
+  def create
+    @history = History.new(history_params)
+
+    if @history.save
+      render json: @history, status: :created
+    else
+      render json: {errors: [@history.errors.full_messages.to_sentence]}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def history_params
+    params.require(:history).permit(:food_id).merge(user_id: current_user.id)
+  end
+end
