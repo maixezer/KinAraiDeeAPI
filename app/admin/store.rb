6a588df555e7bdda850d8b23c3bec42fb3f89cbe
maixezer <maixezer@gmail.com>
2016-05-15 19:40:04 +0700

    Create FoodsStores model for use in the active admin to handle food and stores.

--- app/admin/store.rb
+++ app/admin/store.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register Store do
-  permit_params :name, :lat, :lng
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :lat
+    column :lng
+    column :food_ids
+    actions
+  end
+
+  permit_params :name, :lat, :lng, food_ids: []
+
+  form do |f|
+    f.inputs "Food Details" do
+      f.input :name
+      f.input :lat
+      f.input :lng
+      f.input :food_ids, :as => :check_boxes, :collection => Food.all
+    end
+    f.actions
+  end
 end
